666a7be012110eb62e1d0f1f6d940dc7facdfdff
HalliteCC <hallitecaie@gmail.com>
2023-01-27 16:07:12 -0300

    Start Commit

---
+++ app/src/main/java/com/example/motivationapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.motivationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.motivationapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        handleUserName()
+
+
+        //events
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.buttonNewPhrase){
+            var s = ""
+        }
+    }
+
+    private fun handleUserName (){
+
+        val name = SecurityPreferences(this).getString("User_Name")
+
+        binding.textHello.text = "Olá, $name!"
+    }
+}
